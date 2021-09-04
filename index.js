10a34170162f9eedbe99aad3e6bac254fca164ae
Devon Zuegel <devonzuegel@gmail.com>
2021-09-04 16:18:38 -0400

    Add tests

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import strSimilarity from 'string-similarity'
 import date_fns from 'date-fns'
 import timezone_fns from 'date-fns-tz'
 
+const run_tests = false
+
 var tzNames = [
   'Europe/Andorra',
   'Asia/Dubai',
@@ -403,10 +405,6 @@ const pprint = (obj) => {
   console.log(JSON.stringify(obj, null, 2))
 }
 
-const [local_time, ...cities] = alfy.input
-  .split(alfy.input.includes('/') ? '/' : ' ')
-  .map((s) => s.trim())
-
 /**
  * local_time: fuzzy date with case-insensitive am/pm ("10am", "11:30pm", "7:43AM")
  * returns:    a parsed date that can be manipulated by the datefn libraries
@@ -421,7 +419,7 @@ const get_local_time = (local_time) => {
 
 /**
  * date:    a date that can be manipulateby the datefn libraries
- * returns: a formatted time with minutes when they are non-zero
+ * returns: a fmt time with minutes when they are non-zero
  */
 const format_time = (date) => {
   const format = date_fns.format
@@ -432,13 +430,48 @@ const format_time = (date) => {
   }
 }
 
-const times = cities.map((city) => {
-  const tz = city_to_tz(city)
-  const parsed_local_time = get_local_time(local_time)
-  const zoned_date = timezone_fns.utcToZonedTime(parsed_local_time, tz)
+const toSentenceCase = (str) => {
+  return str
+    .split(' ')
+    .map((w) => w[0].toUpperCase() + w.substr(1).toLowerCase())
+    .join(' ')
+}
 
-  const formatted_date = format_time(zoned_date)
-  return formatted_date + ' ' + city
-})
+const tz_expander = (input, test) => {
+  const [local_time, ...cities] = input
+    .split(input.includes('/') ? '/' : ' ')
+    .map((s) => s.trim())
 
-console.log(times.join(' / '))
+  const times = cities.map((city) => {
+    const tz = city_to_tz(city)
+    const parsed_local_time = get_local_time(local_time)
+    const zoned_date = timezone_fns.utcToZonedTime(parsed_local_time, tz)
+
+    const fmt_date = format_time(zoned_date)
+    const fmt_city = city.length == 2 ? city.toUpperCase() : toSentenceCase(city)
+    return fmt_date + ' ' + fmt_city
+  })
+
+  if (!test) console.log(times.join(' / '))
+  return times.join(' / ')
+}
+
+if (run_tests) {
+  const tests = [
+    ['10:15am PT ET', '7:15am PT / 10:15am ET'],
+    ['9AM PT ET', '6am PT / 9am ET'],
+    ['9AM / PT / ET', '6am PT / 9am ET'],
+    ['9am/ PT /ET', '6am PT / 9am ET'],
+    ['9am/PT/ET', '6am PT / 9am ET'],
+    ['9am London', '2pm London'],
+    ['9am UK', '2pm UK'],
+    ['2pm/pt/et', '11am PT / 2pm ET'],
+    ['2pm/SF/miami', '11am SF / 2pm Miami'],
+  ]
+
+  tests.map(([input, output]) => {
+    console.log(tz_expander(input, true) == output)
+  })
+} else {
+  tz_expander(alfy.input)
+}
